dbabc82142646dfe2961d10611c38de3721c6a40
tserkov <james.churchard@gmail.com>
2017-12-29 15:56:12 -0600

    updated to showcase reactive options

--- src/components/PageOne.vue
+++ src/components/PageOne.vue
@@ -1,9 +1,14 @@
 <template>
   <main>
-    <section v-scroll-reveal="{ delay: 250 }">
+    <section v-scroll-reveal.reset="{ delay }">
       <img src="../assets/logo.png"><br>
 
-      <router-link :to="{ name: 'PageTwo' }">Page Two</router-link>
+      <router-link :to="{ name: 'PageTwo' }">Page Two</router-link><br>
+
+      <small>or scroll down</small>
+    </section>
+    <section style="padding:100vh 0 0 0">
+      <small>now scroll back up for a 1s delay!</small>
     </section>
   </main>
 </template>
@@ -11,5 +16,15 @@
 <script>
   export default {
     name: 'PageOne',
+    data() {
+      return {
+        delay: 250,
+      };
+    },
+    mounted() {
+      setTimeout(() => {
+        this.delay = 1000;
+      }, 2500);
+    },
   };
 </script>
